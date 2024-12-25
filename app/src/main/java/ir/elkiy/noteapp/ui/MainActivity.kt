c7dd27b23d8492717d5258cd8f4fd41e43b4ef62
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-12-25 17:08:06 +0330

    fix items

--- app/src/main/java/ir/elkiy/noteapp/ui/MainActivity.kt
+++ app/src/main/java/ir/elkiy/noteapp/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.View.VISIBLE
 import android.widget.Toast
